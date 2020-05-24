bdf75e54e9e0f84a1ffaa136a496466d0c739f0e
mreouven-study <37777368+mreouven-study@users.noreply.github.com>
2020-05-24 20:00:57 +0300

    Create ecosystem.config.js

---
+++ back/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'dist/index.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+
+};
