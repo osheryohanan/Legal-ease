377031b5ae4d8b3b9a5c6afa8d9601b57d45df4e
mreouven-study <37777368+mreouven-study@users.noreply.github.com>
2020-05-24 23:50:22 +0300

    website add

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,51 +1,63 @@
-import express from 'express';
-
-import cors from 'cors';
-
-import bodyParser from 'body-parser';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import { userRoute } from "./routes/user.route";
 import { commentsRoute } from "./routes/comments.route";
-import { InitiateMongoServer } from './config/db';
-require('dotenv').config();
-
-
-import { errorHandler  } from "./interfaces/error.interfaces";
-
-
-
-const app=express();
+import { InitiateMongoServer } from "./config/db";
+import fs from "fs";
+import path from "path";
+import { errorHandler } from "./interfaces/error.interfaces";
+require("dotenv").config();
+const app = express();
 
 //json -POST
 app.use(bodyParser.json());
 //urlencoded -POST
-app.use(bodyParser.urlencoded(
-    {extended:true}
-));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(cors());
-
 
 InitiateMongoServer();
 
+app.use("/user", new userRoute().router);
+app.use("/comments", new commentsRoute().router);
 
-app.use('/user',new userRoute().router);
-app.use('/comments',new commentsRoute().router);
 
-app.all("*",(req,res)=>{
-    var error:errorHandler={
-        status:404,
-        message:`We can't access to this path`,
-        type:'Invalid Path'
+
+var website=express();
+website.get('*', (req, res) => {
+try {
+  if (fs.existsSync(path.join(__dirname, "../../", "front/dist/lease-ease/index.html"))) {
+    const allowedExt = ['.js', '.ico', '.css', '.png', '.jpg', '.woff2', '.woff', '.ttf', '.svg', '.gif', '.map'];
+    // if (req.ip != '::ffff:37.142.6.113') {
+    //   return res.send('you are unauthorized to access this resource from the ip '+req.ip);
+    // }
+    if (allowedExt.filter((ext) => req.url.indexOf(ext) > 0).length > 0) {
+      return res.sendFile(path.join(__dirname,"../../" ,'front/dist/lease-ease/' ,`${req.url}`));
+    } else {
+      return res.sendFile(path.join(__dirname, "../../", "front/dist/lease-ease/index.html"));
     }
-    res.status(error.status).send(error);
+  }
+  throw "err";
+} catch (error) {
+  res.send("can't resolve front");
+}
+});
+app.use('/',website);
+
+
+
+app.all("*", (req, res) => {
+  var error: errorHandler = {
+    status: 404,
+    message: `We can't access to this path`,
+    type: "Invalid Path",
+  };
+  res.status(error.status).send(error);
 });
 
-
-
-
-
-const PORT=process.env.PORT||8080;
-app.listen(PORT,()=>{
-    console.log('Listen on port '+PORT);
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log("Listen on port " + PORT);
+});
