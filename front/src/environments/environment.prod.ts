22778ea3c217da96cf96e4737948a8e3b7416186
mreouven-study <37777368+mreouven-study@users.noreply.github.com>
2020-07-22 12:21:50 +0300

    Update environment.prod.ts

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL:'https//api.legal-ease.co.il/',
+  apiURL:'https://api.legal-ease.co.il/',
   GID:"802321656680-tu39g3vtn7qnf0itslvs1969eiv1f169.apps.googleusercontent.com",
 
 };
