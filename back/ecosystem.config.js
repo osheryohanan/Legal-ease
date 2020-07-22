2bfe78c03a423df04bfcec3d5859a26eb1ea6f23
mreouven-study <37777368+mreouven-study@users.noreply.github.com>
2020-07-22 10:53:06 +0300

    ecosystem

--- back/ecosystem.config.js
+++ back/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'API',
-    script: 'dist/index.js',
+    script: 'dist/back/src/index.js',
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
 
